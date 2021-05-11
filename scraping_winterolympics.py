d50a46c81842b7e3dd026ad61b10d393aa49a696
micha <micha.kaeser@stud.hslu.ch>
2021-05-12 00:40:42 +0200

    - documentation added

--- scraping_winterolympics.py
+++ scraping_winterolympics.py
@@ -1,25 +1,36 @@
+# Zuerst werden die benötigten Libraries importiert
 import requests
 from bs4 import BeautifulSoup
 import csv
 
+# Hier sind In- und Output definiert. Als Input die URL, die gescraped wird, und als Output der CSV-Filename des Exports
 url = "https://www.taschenhirn.de/sport/olympische-winterspiele/"
 csv_output_name = "winter_olympics.csv"
 
-# Make a GET request to fetch the raw HTML content
+# Mit einem Get-Request werden die rohen HTML-Daten geladen...
 html_content = requests.get(url).text
 
-# Parse the html content
+# ...und mittels LXML geparsed
 soup = BeautifulSoup(html_content, "lxml")
-#print(soup.prettify()) # print the parsed data of html
 
+# Ein erstes Print des gescrapten HTML-Inhalts gibt uns eine Übersicht
+print(soup.prettify())
+
+# Als -olympics_table wird der Inhalt des Tags <table> mit der Klasse "dataList" geladen
+# Danach wird -olympics_table nach allen Tags <tr> durchsucht
 winter_olympics_table = soup.find("table", attrs={"class": "dataList"})
 winter_olympics_table_data = winter_olympics_table.tbody.find_all("tr")
 
+# Damit die Daten bzw. Kolonnen auch benennt werden, scrapen wir hier die Überschriften der Tabelle
 headers = []
 for i in winter_olympics_table.find_all('th'):
     title = i.text.strip()
     headers.append(title)
 
+# Wir lassen uns die geladenen Überschriften ausgeben
+print(headers)
+
+# In _olympics_table_data werden nun alle Tags <td> gesucht. Diese beinhalten pro Datensatz einen Wert (Jahr, Ort etc.)
 rows = []
 for row in winter_olympics_table_data:
     value = row.find_all('td')
@@ -29,6 +40,10 @@ for row in winter_olympics_table_data:
         continue
     rows.append(beautified_value)
 
+# Die Kolonnen werden ausgegeben
+print(rows)
+
+# Schlussendlich wird unter dem eingangs definitierten Namen eine CSV-Datei exportiert
 with open(csv_output_name, 'w', newline="") as output:
     writer = csv.writer(output)
     writer.writerow(headers)
