2075acb52082da106c3e839b00abd519d49923de
regis <regis.andreoli@gmail.com>
2021-05-16 00:18:33 +0200

    code corrections and improvements

--- fragestellung_2.py
+++ fragestellung_2.py
@@ -6,13 +6,8 @@
 #### laypout df: kontinent, ländername, jahr, bip
 
 #load libraries and set genreals
-import warnings
-warnings.filterwarnings('ignore')
-from distutils.version import StrictVersion
 import pandas as pd
-assert StrictVersion(pd.__version__) >= StrictVersion('0.19.0')
-import seaborn as sns
-assert StrictVersion(sns.__version__) >= StrictVersion('0.7.0')
+from matplotlib import pyplot as plt
 
 #load data
 df_c = pd.read_csv('country_stage.csv', header=0, encoding='utf-8')
@@ -22,6 +17,7 @@ df_r = pd.read_csv('rgdpna.csv', header=0, encoding='utf-8')
 
 #first -> generals and irregularities inspection
 df_c.head()
+df_c.info()
 df_k.head()
 df_r.head()
 df_r.count()
@@ -30,13 +26,47 @@ df_r.info()
 #drop superfluous column
 df_r = df_r.drop(columns="VariableCode")
 
-#join table together
+#join all tables together
+df_joined_1 = pd.merge(df_c, df_k, on='Land', how='inner')
+df_joined_1.head()
+df_joined_1.info() #-> loss of ~ 60 entries. depper look at "länderkode_stage.csv"...
+# revealed that the naming of the country is not consistentent at all
+# thus, a manually review is necessary.
+################################################################################################
+#### input: länderkode_stage.csv
+#### process: manual correction
+#### output: länderkode_stage_v2.csv
+
+#find difference: df_c is the "good" data set
+for l in df_k['Land']:
+    if not df_c['Land'].str.contains(l).any():
+        print(l)
+
+df_k = pd.read_csv('länderkode_stage_v2.csv', header=0, encoding='utf-8')
+#-> loss of ~ 20 entries. no impactfull country left
+
 df_joined_1 = pd.merge(df_c, df_k, on='Land', how='inner')
 df_joined_1.head()
 df_joined_1.info()
+################################################################################################
+#go on with merging
 df_joined_2 = pd.merge(df_joined_1, df_r, left_on='ISO-3', right_on='RegionCode', how='inner')
 df_joined_2.head()
 df_joined_2.info()
+
+#index the df and group index with sum of column value
+df_i2 = df_joined_2.set_index(['Kontinent', 'YearCode'])
+new_df2 = df_i2.groupby(level= ['Kontinent', 'YearCode']).sum()
+
+#reset the index to columns
+df_res = new_df.reset_index()
+df_res2 = new_df2.reset_index()
+df_res
+df_res2.info()
+
+#first time-gdp analysis
+df_piv = df_res2.pivot(index='YearCode', columns='Kontinent', values='AggValue')
+df_piv.plot()
 
 #drop , resp. keep only row with year 2000 and 2019
 df_j = df_joined_2.drop(df_joined_2[(df_joined_2.YearCode != 2000) & (df_joined_2.YearCode != 2019)].index)
@@ -44,20 +74,13 @@ df_j.head()
 df_j.info()
 df_j
 
-#index the df
+#index the df and group index with sum of column value
 df_i = df_j.set_index(['Kontinent', 'YearCode'])
-
-#group index with sum of column value
 new_df = df_i.groupby(level= ['Kontinent', 'YearCode']).sum()
 
-#reset the index to columns
-df_res = new_df.reset_index()
-df_res
-
-
-df_res_2 = df_res.set_index(['Kontinent'])
-df_res_2
-#
-for i in df_res["AggValue"]:
-    x - i
-    x = i
+#first analysis
+df_res.plot(x='YearCode', y='AggValue', columns=['Kontinent'],  marker='o', color='goldenrod', linewidth=3.0, figsize=(20,10))
+plt.xlabel('time')
+plt.ylabel('USD')
+plt.legend()
+plt.title('Contintental GDP 2000 vs 2019')
