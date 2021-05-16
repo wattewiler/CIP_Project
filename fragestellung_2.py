2167332f156ca837f0e98ddb15e9b9184a5c8d50
regis <regis.andreoli@gmail.com>
2021-05-16 14:56:56 +0200

    code corrections and improvements

--- fragestellung_2.py
+++ fragestellung_2.py
@@ -15,12 +15,12 @@ df_k = pd.read_csv('länderkode_stage.csv', header=0, encoding='utf-8')
 df_r = pd.read_csv('rgdpna.csv', header=0, encoding='utf-8')
 
 
-#first -> generals and irregularities inspection
+#first generals inspection
 df_c.head()
 df_c.info()
 df_k.head()
+df_k.info()
 df_r.head()
-df_r.count()
 df_r.info()
 
 #drop superfluous column
@@ -29,32 +29,35 @@ df_r = df_r.drop(columns="VariableCode")
 #join all tables together
 df_joined_1 = pd.merge(df_c, df_k, on='Land', how='inner')
 df_joined_1.head()
-df_joined_1.info() #-> loss of ~ 60 entries. depper look at "länderkode_stage.csv"...
-# revealed that the naming of the country is not consistentent at all
-# thus, a manually review is necessary.
+df_joined_1.info()
+
+# -> loss of ~ 60 entries. deeper look at "länderkode_stage.csv"...
+# revealed that the naming of the country is not consistent at all,...
+# thus, a manual review is necessary.
 ################################################################################################
 #### input: länderkode_stage.csv
 #### process: manual correction
 #### output: länderkode_stage_v2.csv
 
-#find difference: df_c is the "good" data set
+#find difference - "comparing country_stage" with "länderkode_stage"
 for l in df_k['Land']:
     if not df_c['Land'].str.contains(l).any():
         print(l)
 
 df_k = pd.read_csv('länderkode_stage_v2.csv', header=0, encoding='utf-8')
-#-> loss of ~ 20 entries. no impactfull country left
+df_k.info()
+#-> loss of ~ 20 entries. no impactfull country left out
 
 df_joined_1 = pd.merge(df_c, df_k, on='Land', how='inner')
 df_joined_1.head()
 df_joined_1.info()
 ################################################################################################
-#go on with merging
+#go on with merging with new manually corrected csv-file
 df_joined_2 = pd.merge(df_joined_1, df_r, left_on='ISO-3', right_on='RegionCode', how='inner')
 df_joined_2.head()
 df_joined_2.info()
 
-#index the df and group index with sum of column value
+#reindexing the df and group by index, with sum of column value
 df_i2 = df_joined_2.set_index(['Kontinent', 'YearCode'])
 new_df2 = df_i2.groupby(level= ['Kontinent', 'YearCode']).sum()
 
@@ -62,7 +65,7 @@ new_df2 = df_i2.groupby(level= ['Kontinent', 'YearCode']).sum()
 df_res2 = new_df2.reset_index()
 df_res2.info()
 
-#first time-gdp analysis
+#first data analyis: gdp-time by continent
 df_piv = df_res2.pivot(index='YearCode', columns='Kontinent', values='AggValue')
 df_piv.plot()
 
@@ -72,7 +75,7 @@ df_j = df_joined_2.drop(df_joined_2[(df_joined_2.YearCode != 2000) & (df_joined_
 df_j.head()
 df_j.info()
 
-#index the df and group index with sum of column value
+#reindexing the df and group by index, with sum of column values
 df_i = df_j.set_index(['Kontinent', 'YearCode'])
 new_df = df_i.groupby(level= ['Kontinent', 'YearCode']).sum()
 #reset the index to columns
@@ -110,22 +113,22 @@ b = {"Afrika": [delta_gdp_r("Afrika")],
      }
 b_df = pd.DataFrame(b)
 
-#invert index with column, and sort gdp ascending
+#invert index with column, and sort gdp (ascending)
 xx = b_df.T
 x1 = xx.sort_values(by=0)
 x2 = x1.reset_index()
 x2.columns = ["Kontinent", "GDP"]
 x2.plot.bar(x='Kontinent', y='GDP', color = "green")
-plt.title('absolute change of GDP')
+plt.title('relative change of GDP')
 
 yy = a_df.T
 y1 = yy.sort_values(by=0)
 y2 = y1.reset_index()
 y2.columns = ["Kontinent", "GDP"]
 y2.plot.bar(x='Kontinent', y='GDP', color = "gold")
-plt.title('relative change of GDP')
+plt.title('absolut change of GDP')
 
 
 ### lessons learned
-#scientific notation (3.45e+08), stored in a text type, can be correctly interpreted...
-# by converting it as float number
+#scientific notation (3.45e+08), stored as a text type, can be correctly interpreted...
+# by converting it to a float number
